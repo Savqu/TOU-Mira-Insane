5542ff6d5090a5c7d2dc3f809f30a2bf2b75ece1
GraczBezNicku <58118380+GraczBezNicku@users.noreply.github.com>
2025-08-17 15:38:48 +0200

    Finished Insane Mystic

--- TownOfUs/Modifiers/Game/Universal/InsaneModifier.cs
+++ TownOfUs/Modifiers/Game/Universal/InsaneModifier.cs
@@ -2,17 +2,32 @@
 using MiraAPI.Modifiers;
 using MiraAPI.Utilities;
 using Reactor.Networking.Attributes;
+using Reactor.Utilities;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TownOfUs.Modifiers.Crewmate;
+using UnityEngine;
 
 namespace TownOfUs.Modifiers.Game.Universal;
 
 public class InsaneModifier : BaseModifier
 {
     public override string ModifierName => "Insane";
+
+    public void AddMysticDeathWithDelay(PlayerControl target, PlayerControl mystic)
+    {
+        IEnumerator AddWithDelay()
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(0f, 10f));
+            target?.AddModifier<MysticDeathNotifierModifier>(mystic);
+        }
+
+        Coroutines.Start(AddWithDelay());
+    }
 
     [MethodRpc((uint)TownOfUsRpc.SetInsane, SendImmediately = true)]
     public static void SetInsanePlayer(PlayerControl target)
