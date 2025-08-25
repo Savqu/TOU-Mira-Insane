aadaaea52161158a6acaec7dc9e34cef999ebf3e
GraczBezNicku <58118380+GraczBezNicku@users.noreply.github.com>
2025-08-25 11:48:29 +0200

    Added missing icons

--- TownOfUs/Assets/TouModifierIcons.cs
+++ TownOfUs/Assets/TouModifierIcons.cs
@@ -5,6 +5,8 @@ namespace TownOfUs.Assets;
 
 public static class TouModifierIcons
 {
+    private static readonly string iconPath = "TownOfUs.Resources.ModifierIcons";
+
     public static LoadableAsset<Sprite> Aftermath { get; } = new LoadableBundleAsset<Sprite>("Aftermath", TouAssets.MainBundle);
     public static LoadableAsset<Sprite> Bait { get; } = new LoadableBundleAsset<Sprite>("Bait", TouAssets.MainBundle);
     public static LoadableAsset<Sprite> ButtonBarry { get; } = new LoadableBundleAsset<Sprite>("ButtonBarry", TouAssets.MainBundle);
@@ -27,7 +29,19 @@ public static class TouModifierIcons
     public static LoadableAsset<Sprite> Telepath { get; } = new LoadableBundleAsset<Sprite>("Telepath", TouAssets.MainBundle);
     public static LoadableAsset<Sprite> Underdog { get; } = new LoadableBundleAsset<Sprite>("Underdog", TouAssets.MainBundle);
 
-    public static LoadableAsset<Sprite> Insane { get; } = new LoadableResourceAsset($"Insane", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Flash { get; } = new LoadableBundleAsset<Sprite>("Flash", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Giant { get; } = new LoadableBundleAsset<Sprite>("Giant", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Immovable { get; } = new LoadableBundleAsset<Sprite>("Immovable", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Lover { get; } = new LoadableBundleAsset<Sprite>("Lover", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Mini { get; } = new LoadableBundleAsset<Sprite>("Mini", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Radar { get; } = new LoadableBundleAsset<Sprite>("Radar", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Satellite { get; } = new LoadableBundleAsset<Sprite>("Satellite", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Shy { get; } = new LoadableBundleAsset<Sprite>("Shy", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> SixthSense { get; } = new LoadableBundleAsset<Sprite>("SixthSense", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Sleuth { get; } = new LoadableBundleAsset<Sprite>("Sleuth", TouAssets.MainBundle);
+    public static LoadableAsset<Sprite> Tiebreaker { get; } = new LoadableBundleAsset<Sprite>("Tiebreaker", TouAssets.MainBundle);
+
+    public static LoadableAsset<Sprite> Insane { get; } = new LoadableResourceAsset($"{iconPath}.Insane.png");
 
     public static LoadableAsset<Sprite> FirstRoundShield { get; } =
         new LoadableBundleAsset<Sprite>("FirstRoundShield", TouAssets.MainBundle);
